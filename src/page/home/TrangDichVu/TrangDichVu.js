337d8be5bfce753a90ba7df372e5722c4f74e536
Duc <congduc1199@gmail.com>
2020-03-07 09:03:11 +0700

    sdaf

--- src/page/home/TrangDichVu/TrangDichVu.js
+++ src/page/home/TrangDichVu/TrangDichVu.js
@@ -1,11 +1,37 @@
-import React, { Component } from 'react'
-
-export default class TrangDichVu extends Component {
-    render() {
-        return (
-            <div>
-                Day la trang dich vu
-            </div>
-        )
-    }
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as action from "../../../redux/action/index";
+class TrangDichVu extends Component {
+  componentDidMount() {
+    this.props.getDataSearchApi("house");
+  }
+  renderDataSearch = () => {
+    return this.props.dataSearch.map((item, index) => {
+      return (
+        <div
+          key={index}
+          dangerouslySetInnerHTML={{ __html: item.fields.shorttext }}
+        ></div>
+      );
+    });
+  };
+  render() {
+    return (
+      <div>{this.props.dataSearch.length ? this.renderDataSearch() : ""}</div>
+    );
+  }
 }
+
+const mapStateToProps = state => {
+  return {
+    dataSearch: state.deMoReducer.dataSearch
+  };
+};
+const mapDispatchToProps = dispatch => {
+  return {
+    getDataSearchApi: data => {
+      dispatch(action.getDataSearchApi(data));
+    }
+  };
+};
+export default connect(mapStateToProps, mapDispatchToProps)(TrangDichVu);
--- src/redux/action/index.js
+++ src/redux/action/index.js
@@ -1,3 +1,13 @@
 import * as Actiontype from "./../constants/actionType";
+import { CallAPI } from "../../utils/callApi";
 
-
+export const getDataSearchApi = data => {
+  return dispatch => {
+    CallAPI(`WBBcwnwQpV89/a/${data}`).then(rs => {
+      dispatch({
+        type: Actiontype.GET_DATA_SEARCH,
+        dataSearch: rs.data.tratu
+      });
+    });
+  };
+};

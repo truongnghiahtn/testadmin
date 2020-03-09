dab1c1274c874a34bb9e67f99af02612bb296592
pphi <s2bongma2s@gmail.com>
2020-03-09 15:49:18 +0700

    a

--- src/page/admin/MoviesAdmin/MoviesAdmin.js
+++ src/page/admin/MoviesAdmin/MoviesAdmin.js
@@ -1,5 +1,4 @@
-import React, { Component } from "react";
-import HeaderAdmin from "./../../../components/headerAdmin/headerAdmin";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import ItemTable from "./itemTableMovies";
 import ChildModal from "./childModalMovies";
@@ -7,43 +6,38 @@ import Modalfather from "../../../components/modal/fatherModal";
 import * as action from "../../../redux/action/index";
 import SearchAdmin from "../../../components/SearchAdmin";
 const Modal = Modalfather(ChildModal);
-class MoviesAdmin extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      listMovies: []
+
+const MoviesAdmin = props => {
+  const [listMovies, setListMovies] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemPerPage, setItemPerPage] = useState(10);
+
+  useEffect(() => {
+    /*     const fetchPosts = async () => {
+      setLoading(true);
+      const res = await props.getListMovies();
+      console.log(res, 1);
+      setLoading(false);
     };
-  }
+    fetchPosts(); */
+    props.getListMovies();
+  }, []);
 
-  componentDidMount() {
-    this.props.getListMovies();
-  }
+  useEffect(() => {
+    setListMovies(props.listMovies.result);
+  }, [props.listMovies.result]);
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps && nextProps.listMovies) {
-      this.setState({
-        listMovies: nextProps.listMovies.result
-      });
-    }
-  }
-
-  renderTbody = () => {
-    return this.state.listMovies.map((item, index) => (
-      <ItemTable movie={item} stt={index} key={index} />
-    ));
-  };
-
-  handleFilter = keyword => {
-    let listMovies = this.props.listMovies.result.filter(
+  const handleFilter = keyword => {
+    let listMovies = props.listMovies.result.filter(
       item => item.title.toLowerCase().indexOf(keyword.toLowerCase()) > -1
     );
-
-    this.setState({ listMovies });
+    setListMovies({ listMovies });
   };
 
-  renderPageItem = () => {
-    let numberPage = 0;
-    return this.state.listMovies.map((item, index) => {
+  const renderPageItem = () => {
+    /* let numberPage = 0;
+    return listMovies.map((item, index) => {
       if (index % 10 === 0) {
         numberPage++;
         return (
@@ -54,85 +48,97 @@ class MoviesAdmin extends Component {
           </li>
         );
       }
-    });
+    }); */
   };
 
-  render() {
-    return (
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col-sm-12">
-            <div className="iq-card">
-              <div className="iq-card-header d-flex justify-content-between">
-                <div className="iq-header-title">
-                  <h4 className="card-title">
-                    <i
-                      className="fa fa-user-circle"
-                      aria-hidden="true"
-                      style={{ marginRight: 10 }}
-                    />
-                    Phim
-                  </h4>
-                </div>
-                <span
-                  className="table-add float-right mb-3 mr-2"
-                  style={{ marginTop: "1rem!important" }}
-                >
-                  <button
-                    data-toggle="modal"
-                    data-target="#modelId"
-                    className="btn btn-sm iq-bg-success"
-                    onClick={() => this.props.onEditMovie()}
-                  >
-                    <i className="ri-add-fill">
-                      <span className="pl-1">Thêm phim</span>
-                    </i>
-                  </button>
-                </span>
+  const indexOfLastItem = currentPage * itemPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemPerPage;
+  let currentMovies = [];
+  if (listMovies !== undefined) {
+    currentMovies = listMovies.slice(indexOfFirstItem, indexOfLastItem);
+  }
+
+  const renderTbody = () => {
+    if (listMovies !== undefined) {
+      return listMovies.map((item, index) => (
+        <ItemTable movie={item} stt={index} key={index} />
+      ));
+    }
+  };
+  return (
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-sm-12">
+          <div className="iq-card">
+            <div className="iq-card-header d-flex justify-content-between">
+              <div className="iq-header-title">
+                <h4 className="card-title">
+                  <i
+                    className="fa fa-user-circle"
+                    aria-hidden="true"
+                    style={{ marginRight: 10 }}
+                  />
+                  Phim
+                </h4>
               </div>
-              <div className="iq-card-body">
-                <div id="table" className="table-editable">
-                  <div className="iq-search-bar float-right mb-3 mr-2">
-                    <SearchAdmin onFilter={this.handleFilter} />
-                  </div>
-                  <table className="table table-bordered table-responsive-md table-striped text-center">
-                    <thead>
-                      <tr>
-                        <th>STT</th>
-                        <th>Tên phim</th>
-                        <th>Tự đề phim</th>
-                        <th>EN</th>
-                        <th>VI</th>
-                        <th>Chức năng</th>
-                      </tr>
-                    </thead>
-                    <tbody>{this.renderTbody()}</tbody>
-                  </table>
-                  <nav aria-label="Page navigation example">
-                    <ul className="pagination justify-content-end">
-                      <li className="page-item">
-                        <a className="page-link" href="#" aria-label="Previous">
-                          <span aria-hidden="true">«</span>
-                        </a>
-                      </li>
-                      {this.renderPageItem()}
-                      <li className="page-item">
-                        <a className="page-link" href="#" aria-label="Next">
-                          <span aria-hidden="true">»</span>
-                        </a>
-                      </li>
-                    </ul>
-                  </nav>
+              <span
+                className="table-add float-right mb-3 mr-2"
+                style={{ marginTop: "1rem!important" }}
+              >
+                <button
+                  data-toggle="modal"
+                  data-target="#modelId"
+                  className="btn btn-sm iq-bg-success"
+                  onClick={() => props.onEditMovie()}
+                >
+                  <i className="ri-add-fill">
+                    <span className="pl-1">Thêm phim</span>
+                  </i>
+                </button>
+              </span>
+            </div>
+            <div className="iq-card-body">
+              <div id="table" className="table-editable">
+                <div className="iq-search-bar float-right mb-3 mr-2">
+                  <SearchAdmin onFilter={handleFilter} />
                 </div>
+                <table className="table table-bordered table-responsive-md table-striped text-center">
+                  <thead>
+                    <tr>
+                      <th>STT</th>
+                      <th>Tên phim</th>
+                      <th>Tự đề phim</th>
+                      <th>EN</th>
+                      <th>VI</th>
+                      <th>Chức năng</th>
+                    </tr>
+                  </thead>
+                  <tbody>{renderTbody()}</tbody>
+                </table>
+                <nav aria-label="Page navigation example">
+                  <ul className="pagination justify-content-end">
+                    <li className="page-item">
+                      <a className="page-link" href="#" aria-label="Previous">
+                        <span aria-hidden="true">«</span>
+                      </a>
+                    </li>
+                    {renderPageItem()}
+                    <li className="page-item">
+                      <a className="page-link" href="#" aria-label="Next">
+                        <span aria-hidden="true">»</span>
+                      </a>
+                    </li>
+                  </ul>
+                </nav>
               </div>
             </div>
           </div>
         </div>
-        <Modal />
       </div>
-    );
-  }
-}
+      <Modal />
+    </div>
+  );
+};
 
 const mapStateToProps = state => {
   return {
--- src/page/admin/MoviesAdmin/itemTableMovies.js
+++ src/page/admin/MoviesAdmin/itemTableMovies.js
@@ -1,10 +1,6 @@
 import React, { Component } from "react";
 import * as action from "../../../redux/action/index";
 import { connect } from "react-redux";
-import ChildModal from "./childModalMovies";
-import Modalfather from "../../../components/modal/fatherModal";
-
-const Modal = Modalfather(ChildModal);
 
 class itemTable extends Component {
   convertHTML = html => {
